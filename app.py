17651005b584513a70aca607e03cdaef88ac4c7f
hidechika <mthd0918@gmail.com>
2021-09-01 20:46:49 +0900

    つまった

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def logout():
     return redirect('/login')
 
 
-@app.route("/zandaka")
+@app.route("/zandaka", methods=["GET", "POST"])
 def zandaka():
     if "user_id" in session:
         user_id = session["user_id"]
@@ -83,7 +83,7 @@ def zandaka():
         return redirect('/login')
 
 
-@ app.route('/zandaka_update', methods=["POST"])
+@ app.route('/zandaka_update')
 def update():
     user_id = session["user_id"]
     date = request.form.get("date")
@@ -96,7 +96,7 @@ def update():
               (user_id, date, ac, kingaku))
     conn.commit()
     conn.close()
-    return render_template('update.html')
+    return render_template('zandaka_update.html')
 
 
 @app.route('/del', methods=["POST"])
@@ -109,6 +109,26 @@ def del_task():
     conn.commit()
     conn.close()
     return redirect("/zandaka")
+
+
+# @app.route("/shu-shi", methods=["GET", "POST"])
+# def zandaka():
+#     if "user_id" in session:
+#         user_id = session["user_id"]
+#         conn = sqlite3.connect('kakeibo.db')
+#         c = conn.cursor()
+#         c.execute('select name from user_info where id = ?', (user_id,))
+#         user_info = c.fetchone()
+#         c.execute(
+#             'select id, date ,ac ,kingaku from zandaka where user_id = ? and del_flag = 0', (user_id,))
+#         zandaka_list = []
+#         for row in c.fetchall():
+#             zandaka_list.append(
+#                 {"id": row[0], "date": row[1], "ac": row[2], "kingaku": row[3]})
+#         c.close()
+#         return render_template('zandaka.html', user_info=user_info, zandaka_list=zandaka_list)
+#     else:
+#         return redirect('/login')
 
 
 if __name__ == '__main__':
